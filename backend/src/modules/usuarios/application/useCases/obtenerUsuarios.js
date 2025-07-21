8d32164b950540b86e087a0e5eba7e93a7500d30
Luis <luis.gonzales35@unmsm.edu.pe>
2025-07-21 11:37:53 -0500

    21-07-25 11:37am

--- backend/src/modules/usuarios/application/useCases/obtenerUsuarios.js
+++ backend/src/modules/usuarios/application/useCases/obtenerUsuarios.js
@@ -2,10 +2,12 @@ module.exports = async (usuarioRepository) => {
     // ** Llamamos al repositorio para obtener todos los usuarios
     const usuarios = await usuarioRepository.obtenerUsuarios();
 
+    console.log('usuarios', usuarios);
+
     return {
         codigo: 200,
         respuesta: {
-            mensaje: asistencias.length == 0 ? "Usuarios no registrados" : "Usuarios encontrados",
+            mensaje: usuarios.length == 0 ? "Usuarios no registrados" : "Usuarios encontrados",
             estado: true,
             total: usuarios.length,
             datos: usuarios,
