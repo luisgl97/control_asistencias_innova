abbf557efdfaf647c4a84963c9ad016bfd23f576
Dev-Simon20 <jeanpierks6@gmail.com>
2025-08-14 12:20:21 -0500

    feat: aumentado maxZoom del TileLayer en el componente RegistroObras

--- frontend/src/modules/obras/pages/RegistroObras.jsx
+++ frontend/src/modules/obras/pages/RegistroObras.jsx
@@ -212,6 +212,7 @@ const RegistroObras = () => {
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      maxZoom={22}
                 />
                 {
                     form.latitud && form.longitud && showRadio &&
